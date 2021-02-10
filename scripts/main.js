70468d55921a42c35ca1514ef8fa07e7b49610fb
michael moore <michael.moore@hey.com>
2021-02-10 13:01:36 -0500

    updated on 02-10-20

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', function() {
   // Execute after page load
-  function deal(target, deck) { 
+  function deal(deck, target) { 
     const currentCard = deck.pop()
     target.push(currentCard)
-}})
+}
 
 function getCardImage(card){
   const cardImage = document.createElement('img');
@@ -22,22 +22,6 @@ function getCardImage(card){
   return cardImage
 }
 
-
-const dealButton = document.querySelector('#deal-button')
-// const dealerHand = document.querySelector('#dealer-hand')
-// const playerHand = document.querySelector('#player-hand')
-
-let dealerHand = [];
-let playerHand = [];
-
-dealButton.addEventListener("click", (e) => {
-  dealButton.setAttribute("disabled", true)
-  deal(deck, playerhand);
-  deal(deck, deckhand);
-  deal(deck, playerhand);
-  deal(deck, deckhand);
-})
-
 // lines 23 - 33 generate your deck
 function generateDeck() {
   let deck = [];
@@ -52,5 +36,86 @@ function generateDeck() {
       deck.push(card)
     }
   }
-return deck
+  shuffle(deck)
+  return deck
 } 
+
+function render() {
+  const dealerHandElement = document.querySelector('#dealer-hand')
+  const playerHandElement = document.querySelector('#player-hand')
+  
+
+  for (let index = 0; index < dealerHand.length; index++) {
+    const card = dealerHand[index];
+    const cardElement = getCardImage(card);
+    dealerHandElement.appendChild(cardElement)
+  } 
+  for (let index = 0; index < playerHand.length; index++) {
+    const card = playerHand[index];
+    const cardElement = getCardImage(card);
+    playerHandElement.appendChild(cardElement)
+  }
+}
+
+
+
+function shuffle(deck) {
+  for (let index = 0; index < deck.length; index++) {
+    const randomIndex = Math.floor(Math.random() * index);
+    const temporary = deck[index]
+    deck[index] = deck[randomIndex]
+    deck[randomIndex] = temporary
+  }
+}
+
+function calculatePoints(deck) {
+  let points = 0;
+  let aces = 0;
+  for (let index = 0; index < deck.length; index++) {
+    const currentCard = deck[index];
+    if (currentCard.rank > 1 && currentCard.rank < 11) {
+      points += currentCard.rank;
+    } else if (currentCard.rank >= 11) {
+      points += 10;
+    } else if (currentCard.rank === 1) {
+      aces++; 
+      points += 11;
+    }
+  }
+  while (points > 21 && aces > 0) {
+    points -= 10;
+    aces--;
+  }
+  return points;
+}
+
+const dealButton = document.querySelector('#deal-button')
+// const dealerHand = document.querySelector('#dealer-hand')
+// const playerHand = document.querySelector('#player-hand')
+
+let dealerHand = [];
+let playerHand = [];
+let deck = generateDeck()
+const hitButton = document.querySelector('#hit-button')
+
+hitButton.addEventListener('click', (e) => {
+  deal(deck, playerHand)
+  render()
+})
+
+dealButton.addEventListener("click", (e) => {
+  dealButton.setAttribute("disabled", true)
+  deal(deck, playerHand);
+  deal(deck, dealerHand);
+  deal(deck, playerHand);
+  deal(deck, dealerHand);
+  const playerPoints = calculatePoints(playerHand);
+  const dealerPoints = calculatePoints(dealerHand);
+  if (playerPoints === 21 && dealerPoints < 21) {
+    message("Player has 21. Player wins!");
+    isGameOver = true;
+  }
+  render()
+})
+
+})
